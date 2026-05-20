604fbfdc0007d152db806803c3a83975737742c1
vishnandaman <avishwak@redhat.com>
2026-05-20 19:32:23 +0530

    added config file which fixes the issue

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  SCRIPT_URL: "https://script.google.com/a/macros/redhat.com/s/AKfycbyK8yAoE9XmJpk6vA78Lgl1zoeSr5Un-HNiSzs-n3vQEhXlZ-BCmtpWar6SxuWXImHsEA/exec",
+  ADMIN_PASS: "skillwar2026",
+  POLL_INTERVAL_VOTER: 8000,
+  POLL_INTERVAL_ADMIN: 5000
+};

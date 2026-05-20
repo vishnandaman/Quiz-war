e7cd49014f4e5839286a921401f1c1cf605fbf4e
vishnandaman <avishwak@redhat.com>
2026-05-20 21:22:07 +0530

    fix:fetch with credentials: 'include' also gets blocked by CORS

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SCRIPT_URL: "https://script.google.com/a/macros/redhat.com/s/AKfycbyK8yAoE9XmJpk6vA78Lgl1zoeSr5Un-HNiSzs-n3vQEhXlZ-BCmtpWar6SxuWXImHsEA/exec",
+  SCRIPT_URL: "https://script.google.com/a/macros/redhat.com/s/AKfycbzTk3pFOLvsBD8VFxAP0fr2KrqKQRcEW-qB6irewVOMpaR86YEqeXQCw7AA2i7X6ux0lA/exec",
   ADMIN_PASS: "skillwar2026",
   POLL_INTERVAL_VOTER: 8000,
   POLL_INTERVAL_ADMIN: 5000
